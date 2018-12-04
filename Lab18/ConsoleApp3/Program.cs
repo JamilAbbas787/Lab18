a1c6b57946d2b59b54aa2c178dd60942011bc143
Jamil Abbas <jamilabbas787@gmail.com>
2018-12-03 21:45:32 -0500

    First Commit

---
+++ Lab18/ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class LinkedList
+    {
+        public static bool RemoveAt<t> (this LinkedList<t> list, int index)
+        {
+            if (list.ElementAt(index) != null)
+            {
+                list.RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void PrintReverse<t>(this LinkedList<t> list)
+        {
+            list.PrintReverse();
+        }
+
+        public static bool InsertAt (int index, Object o)
+        {
+
+        }
+
+    }
+
+}
