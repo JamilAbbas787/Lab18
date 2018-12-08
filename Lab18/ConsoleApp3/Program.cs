10e8f1a5d1c14036565184c6a60f8be168cc8dab
Jamil Abbas <jamilabbas787@gmail.com>
2018-12-08 16:33:51 -0500

    Using Dictionary

--- Lab18/ConsoleApp3/Program.cs
+++ Lab18/ConsoleApp3/Program.cs
@@ -10,8 +10,47 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 1,2,3,2,2,4,5,5,7,8,4,4,1,0,10};
+            var numbersDistinct = FindDistinct(numbers);
+            foreach (var number in numbersDistinct)
+            {
+                Console.WriteLine($"[{number.Key}]: {number.Value}");
+            }
+                      
+            Console.ReadKey();
+
         }
+
+        public static Dictionary<int,int> FindDistinct(int[] oldArray)
+        {
+            var newDictionaryDistinct = new Dictionary<int,int>();
+            
+            Array.Sort(oldArray);
+            var distinctArray = oldArray.Distinct().ToArray();
+
+            for (int i = 0; i < distinctArray.Length; i++)
+            {
+                int k = 0;
+                for (int j = 0; j < oldArray.Length; j++)
+                {
+                    if (oldArray[j] == distinctArray[i])
+                    {
+                        k++;
+                    }                    
+                   
+                }               
+                        
+                newDictionaryDistinct.Add(distinctArray[i], k);
+                
+               
+            }
+
+            return newDictionaryDistinct;
+        }
+
     }
+
+   
 
     public static class LinkedList
     {
