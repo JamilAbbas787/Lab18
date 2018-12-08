c136a7405b26da9861e091d9b3ff06e364505973
Jamil Abbas <jamilabbas787@gmail.com>
2018-12-08 17:38:10 -0500

    Completed array

--- Lab18/ConsoleApp3/Program.cs
+++ Lab18/ConsoleApp3/Program.cs
@@ -11,17 +11,19 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             int[] numbers = { 1,2,3,2,2,4,5,5,7,8,4,4,1,0,10};
-            var numbersDistinct = FindDistinct(numbers);
+            var numbersDistinct = FindDistinctWithDictionary(numbers);
+            FindDistinctWithArray(numbers);
+            Console.WriteLine("Using a list:  ");
             foreach (var number in numbersDistinct)
             {
                 Console.WriteLine($"[{number.Key}]: {number.Value}");
             }
-                      
+                 
             Console.ReadKey();
 
         }
 
-        public static Dictionary<int,int> FindDistinct(int[] oldArray)
+        public static Dictionary<int,int> FindDistinctWithDictionary(int[] oldArray)
         {
             var newDictionaryDistinct = new Dictionary<int,int>();
             
@@ -42,10 +44,40 @@ namespace ConsoleApp3
                         
                 newDictionaryDistinct.Add(distinctArray[i], k);
                 
-               
+              
             }
 
             return newDictionaryDistinct;
+        }
+
+        public static void FindDistinctWithArray(int[] oldArray)
+        {
+
+            Array.Sort(oldArray);
+            var distinctArray = oldArray.Distinct().ToArray();
+            int arraySize = distinctArray.Length;
+            int[,] newDistinctArray = new int[arraySize, arraySize] ;
+
+            Console.WriteLine("Using a an array:  ");
+            for (int i = 0; i < distinctArray.Length; i++)
+            {
+                int k = 0;
+                for (int j = 0; j < oldArray.Length; j++)
+                {
+                    if (oldArray[j] == distinctArray[i])
+                    {
+                        k++;
+                    }
+
+                }
+
+
+                newDistinctArray[i,0] = distinctArray[i];
+                newDistinctArray[i, 1] = k;
+                Console.WriteLine($"[{distinctArray[i]}]: {k}");
+
+            }
+
         }
 
     }
