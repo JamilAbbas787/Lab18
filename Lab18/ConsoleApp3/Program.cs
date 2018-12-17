20676810ef502fd809fa1a0af9a1440b8d2c2e83
James Jackson <James@Grandcircus.co>
2018-12-16 21:12:17 -0500

    James TA Review

--- Lab18/ConsoleApp3/Program.cs
+++ Lab18/ConsoleApp3/Program.cs
@@ -1,158 +1,160 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] numbers = { 1,2,3,2,2,4,5,5,7,8,4,4,1,0,10};
-            var test = new LinkedList<int>();
-            //foreach (var number in numbers)
-            //{
-            //    test.AddLast(number);
-            //}
-            //LinkedList.PrintReverse(test);
-            FindDistinctWithDictionary(numbers);
-            FindDistinctWithArray(numbers);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = { 1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10 };
+			var test = new LinkedList<int>();
+			//foreach (var number in numbers)
+			//{
+			//    test.AddLast(number);
+			//}
+			//LinkedList.PrintReverse(test);
+			FindDistinctWithDictionary(numbers);
+			FindDistinctWithArray(numbers);
 
 
-            Console.WriteLine("Big-O Estimate: O(N2) for both List and Array algorithm.");
-            Console.ReadKey();
+			Console.WriteLine("Big-O Estimate: O(N2) for both List and Array algorithm.");
+			Console.ReadKey();
 
-        }
+		}
 
-        public static Dictionary<int,int> FindDistinctWithDictionary(int[] oldArray)
-        {
-            var newDictionaryDistinct = new Dictionary<int,int>();
-            
-            Array.Sort(oldArray);
-            var distinctArray = oldArray.Distinct().ToArray();
+		public static Dictionary<int, int> FindDistinctWithDictionary(int[] oldArray)
+		{
+			var newDictionaryDistinct = new Dictionary<int, int>();
 
-            for (int i = 0; i < distinctArray.Length; i++)
-            {
-                int k = 0;
-                for (int j = 0; j < oldArray.Length; j++)
-                {
-                    if (oldArray[j] == distinctArray[i])
-                    {
-                        k++;
-                    }                    
-                   
-                }               
-                        
-                newDictionaryDistinct.Add(distinctArray[i], k);
-                
-              
-            }
+			Array.Sort(oldArray);
+			var distinctArray = oldArray.Distinct().ToArray();
 
-            Console.WriteLine("Using a list:  ");
-            foreach (var number in newDictionaryDistinct)
-            {
-                Console.WriteLine($"[{number.Key}]: {number.Value}");
-            }
+			for (int i = 0; i < distinctArray.Length; i++)
+			{
+				int k = 0;
+				for (int j = 0; j < oldArray.Length; j++)
+				{
+					if (oldArray[j] == distinctArray[i])
+					{
+						k++;
+					}
 
-            Console.ReadKey();
+				}
 
-            return newDictionaryDistinct;
-        }
-
-        public static void FindDistinctWithArray(int[] oldArray)
-        {
-
-            Array.Sort(oldArray);
-            var distinctArray = oldArray.Distinct().ToArray();
-            int arraySize = distinctArray.Length;
-            int[,] newDistinctArray = new int[arraySize, arraySize] ;
-
-            Console.WriteLine("Using a an array:  ");
-            for (int i = 0; i < distinctArray.Length; i++)
-            {
-                int k = 0;
-                for (int j = 0; j < oldArray.Length; j++)
-                {
-                    if (oldArray[j] == distinctArray[i])
-                    {
-                        k++;
-                    }
-
-                }
+				newDictionaryDistinct.Add(distinctArray[i], k);
 
 
-                newDistinctArray[i,0] = distinctArray[i];
-                newDistinctArray[i, 1] = k;
-                Console.WriteLine($"[{distinctArray[i]}]: {k}");
+			}
 
-            }
+			Console.WriteLine("Using a list:  ");
+			foreach (var number in newDictionaryDistinct)
+			{
+				Console.WriteLine($"[{number.Key}]: {number.Value}");
+			}
 
-        }
+			Console.ReadKey();
 
-    }
+			return newDictionaryDistinct;
+		}
 
-   
+		public static void FindDistinctWithArray(int[] oldArray)
+		{
 
-    public static class LinkedList
-    {
-        public static bool RemoveAt<t> (this LinkedList<t> list, int index)
-        {
-            LinkedListNode<t> currentNode = list.First;
-            if (list.ElementAt(index) != null)
-            {
-                for (int i = 0; i < list.Count; i++)
-                {
-                    if (i == index)
-                    {
-                        list.Remove(currentNode);
-                    }
-                    currentNode = currentNode.Next;
-                }
-                                
-            }
-            return false;
-        }
+			Array.Sort(oldArray);
+			var distinctArray = oldArray.Distinct().ToArray();
+			int arraySize = distinctArray.Length;
+			int[,] newDistinctArray = new int[arraySize, arraySize];
+
+			Console.WriteLine("Using a an array:  ");
+			for (int i = 0; i < distinctArray.Length; i++)
+			{
+				int k = 0;
+				for (int j = 0; j < oldArray.Length; j++)
+				{
+					if (oldArray[j] == distinctArray[i])
+					{
+						k++;
+					}
+
+				}
 
 
-        public static void PrintReverse<t>(this LinkedList<t> list)
-        {
-            LinkedList<t> listInReverse = new LinkedList<t>();
-            foreach (var node in list)
-            {
-                listInReverse.AddFirst(node);
-            }
-            foreach (var node in listInReverse)
-            {
-                Console.WriteLine($"{node}");
-            }
-        }
+				newDistinctArray[i, 0] = distinctArray[i];
+				newDistinctArray[i, 1] = k;
+				Console.WriteLine($"[{distinctArray[i]}]: {k}");
 
-        public static bool InsertAt<t> (this LinkedList<t> list , int index, t o)
-        {
-            LinkedListNode<t> currentNode = list.First;
- 
-            if (list.Count >= index)
-            {
-                for (int i = 0; i < list.Count; i++)
-                {
+			}
 
-                    if (i == index - 1 )
-                    {
-                        list.AddAfter(currentNode , o);
-                    }
+		}
 
-                }
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+	}
 
-        }
 
-    }
+
+	// James -- hey Jamil, I would put this in it's own file to keep your code more organized. 
+	// James -- I would also test these methods to make sure they are working.  so far they look fine but I don't 
+	// see any proof of testing.
+	public static class LinkedList
+	{
+		public static bool RemoveAt<t>(this LinkedList<t> list, int index)
+		{
+			LinkedListNode<t> currentNode = list.First;
+			if (list.ElementAt(index) != null)
+			{
+				for (int i = 0; i < list.Count; i++)
+				{
+					if (i == index)
+					{
+						list.Remove(currentNode);
+					}
+					currentNode = currentNode.Next;
+				}
+
+			}
+			return false;
+		}
+
+
+		public static void PrintReverse<t>(this LinkedList<t> list)
+		{
+			LinkedList<t> listInReverse = new LinkedList<t>();
+			foreach (var node in list)
+			{
+				listInReverse.AddFirst(node);
+			}
+			foreach (var node in listInReverse)
+			{
+				Console.WriteLine($"{node}");
+			}
+		}
+
+		// JAmes - I like that you are using generics here, good stuff. 
+		public static bool InsertAt<t>(this LinkedList<t> list, int index, t o)
+		{
+			LinkedListNode<t> currentNode = list.First;
+
+			if (list.Count >= index)
+			{
+				for (int i = 0; i < list.Count; i++)
+				{
+
+					if (i == index - 1)
+					{
+						list.AddAfter(currentNode, o);
+					}
+
+				}
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+
+	}
 
 }
